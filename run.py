dcf2156789740de2a062c7fa3f49ad9f0ec02957
QwiglyDee <qwiglydee@gmail.com>
2024-03-24 17:15:28 +0700

    adjusted for deploy with root_path

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from app import app
 
 uvicorn.run(
     app,
-    host=environ.get('HOST', "0.0.0.0"),
-    port=int(environ.get('PORT', 8000)),
+    host=environ.get("HOST", "0.0.0.0"),
+    port=int(environ.get("PORT", 8000)),
+    root_path=environ.get("ROOT_PATH", ""),
 )
